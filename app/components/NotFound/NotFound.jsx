6f359deb48d333426ecf44933e1ecaf46f12d37c
Aleksandr <exte92@gmail.com>
2022-07-01 15:01:16 +0300

    up not found

---
+++ app/components/NotFound/NotFound.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import NotFoundIcon from '@assets/not-found.svg?jsx';
+
+import './NotFound.scss';
+
+const NotFound = () => {
+    return <div className="empty-icon-container">
+        <div className="animation-container">
+            <div className="bounce"></div>
+            <div className="pebble1"></div>
+            <div className="pebble2"></div>
+            <div className="pebble3"></div>
+        </div>
+        <div>
+            <h2>Page not Found</h2>
+            <p>Sorry! We couldn't find any results.</p>
+        </div>
+
+    </div>
+}
+
+export default NotFound;
